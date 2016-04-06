923d88b96de15027464bce5af96d0a362b5d7d92
LaustN <laustn@gmail.com>
2016-04-06 20:34:59 +0200

    deferring js execution untill document is ready

--- democraticdj/scripts/fiddle.js
+++ democraticdj/scripts/fiddle.js
@@ -1,55 +1,58 @@
+var init = function() {
 // find template and compile it
-var templateSource = document.getElementById('results-template').innerHTML,
+  var templateSource = document.getElementById('results-template').innerHTML,
     template = Handlebars.compile(templateSource),
     resultsPlaceholder = document.getElementById('results'),
     playingCssClass = 'playing',
     audioObject = null;
 
-var fetchTracks = function (albumId, callback) {
+  var fetchTracks = function(albumId, callback) {
     $.ajax({
-        url: 'https://api.spotify.com/v1/albums/' + albumId,
-        success: function (response) {
-            callback(response);
-        }
+      url: 'https://api.spotify.com/v1/albums/' + albumId,
+      success: function(response) {
+        callback(response);
+      }
     });
-};
+  };
 
-var searchAlbums = function (query) {
+  var searchAlbums = function(query) {
     $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-            q: 'artist:' + query,
-            type: 'album',
-            market: "US"
-        },
-        success: function (response) {
-            resultsPlaceholder.innerHTML = template(response);
-        }
+      url: 'https://api.spotify.com/v1/search',
+      data: {
+        q: 'artist:' + query,
+        type: 'album',
+        market: "US"
+      },
+      success: function(response) {
+        resultsPlaceholder.innerHTML = template(response);
+      }
     });
-};
+  };
 
-results.addEventListener('click', function(e) {
+  results.addEventListener('click', function(e) {
     var target = e.target;
     if (target !== null && target.classList.contains('cover')) {
-        if (target.classList.contains(playingCssClass)) {
-            audioObject.pause();
-        } else {
-            if (audioObject) {
-                audioObject.pause();
-            }
-            fetchTracks(target.getAttribute('data-album-id'), function(data) {
-                audioObject = new Audio(data.tracks.items[0].preview_url);
-                audioObject.play();
-                target.classList.add(playingCssClass);
-                audioObject.addEventListener('ended', function() {
-                    target.classList.remove(playingCssClass);
-                });
-                audioObject.addEventListener('pause', function() {
-                    target.classList.remove(playingCssClass);
-               });
-            });
+      if (target.classList.contains(playingCssClass)) {
+        audioObject.pause();
+      } else {
+        if (audioObject) {
+          audioObject.pause();
         }
+        fetchTracks(target.getAttribute('data-album-id'), function(data) {
+          audioObject = new Audio(data.tracks.items[0].preview_url);
+          audioObject.play();
+          target.classList.add(playingCssClass);
+          audioObject.addEventListener('ended', function() {
+            target.classList.remove(playingCssClass);
+          });
+          audioObject.addEventListener('pause', function() {
+            target.classList.remove(playingCssClass);
+          });
+        });
+      }
     }
-});
+  });
 
-searchAlbums('Leonard Cohen');
+  searchAlbums('Leonard Cohen');
+}
+$(document).ready(init);
